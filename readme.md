7da05ba31479404ae39cfa3512cc0fa0a87a08ac
Ak <atulkanswal@gmail.com>
2019-02-04 00:01:49 +0530

    [Search User Changes]

--- readme.md
+++ readme.md
@@ -4,4 +4,5 @@ gem install rack
 gem install sequel
 gem install mysql2
 create database rack_development_database;
+Search Endpoint - http://localhost:9292/search/?keyword=tes
 
--- search_user.rb
+++ search_user.rb
@@ -8,22 +8,32 @@ DB = Sequel.connect(adapter: :mysql2,
         database: "rack_development_database",
         host: 'localhost',
         user: 'root',
-        password: "password")
+        password: "infi@123")
+
 class SearchUser
   def call(env)
     req =  Rack::Request.new(env)
-    if req.get?
-      setup_database
-      search(req)
-    else
-      [404, { "Content-Type" => "application/json"}, []]
-    end
+    route(req)
   end
 
   def search(req)
     users = DB[:users]
     user_names = users.where(Sequel.like(:name, "#{req.params['keyword']}%")).map(:name)
     [ 200, { "Content-Type" => "application/json" }, [{ users: user_names }.to_json] ]
+  end
+
+  def route(req)
+    method = req.path.delete("/")
+    begin
+      if req.get? && self.respond_to?(method)
+        setup_database
+        self.send(method, req)
+      else
+        [404, { "Content-Type" => "application/json"}, []]
+      end
+    rescue
+      [500, {"Content-Type" => "application/json"}, []]
+    end
   end
 
   def setup_database
